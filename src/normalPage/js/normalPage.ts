aa0d45eefafc22fa74e611d10b7aa95f0bac6174
lindh04 <47460872+lindh04@users.noreply.github.com>
2021-08-02 15:28:45 +0200

    remove normalPage

--- src/normalPage/js/normalPage.ts
+++
@@ -1,8 +0,0 @@
-function setParent(el: HTMLElement, newParent: HTMLElement) {
-  newParent.appendChild(el);
-}
-
-setParent(
-  document.querySelector("#myToolsBox") as HTMLElement,
-  document.querySelector("#ctl00_bodyContainer") as HTMLElement
-);
